24d8d2b2e114ad0dea3bed06fee60b88d53f6d20
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-20 13:59:26 -0500

    Hooks version

--- src/AppFunctionComponent.js
+++ src/AppFunctionComponent.js
@@ -6,16 +6,29 @@ import "./App.css"
 
 const App = () => {
   const [monsters, setMonsters] = useState([])
+  const [searchField, setSearchField] = useState("")
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => setMonsters(users))
+      .then((users) => setMonsters(() => users))
   }, [])
+
+  useEffect(() => {
+    console.log(searchField)
+  })
 
   return (
     <div className="App">
       <h1>Function Component App with React Hooks</h1>
+      <input
+        type="search"
+        onChange={(event) =>
+          setSearchField(() => {
+            return event.target.value
+          })
+        }
+      />
       <CardList monsters={monsters}></CardList>
     </div>
   )
