014b6599a395cc9058b9c9081bf14ca84c19904b
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-20 13:46:56 -0500

    Added function components

---
+++ src/components/card/card.function-component.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import "./card.styles.css"
+
+const Card = ({ monster }) => {
+  return (
+    <div className="card-container" key={monster.id}>
+      <img
+        alt={`Monster #${monster.id}`}
+        src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+      />
+      <h2>{monster.name}</h2>
+      <p>{monster.email}</p>
+    </div>
+  )
+}
+
+export default Card
